0dddf4e016ae41b73cfa5f0cb58787b3a5b9e36a
Luis Boscan <luife642@gmail.com>
2017-07-12 22:17:01 -0400

    public folder added

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+// (function () {
+    var socket = io();
+    socket.on('connect', function () {
+        console.log('Connected to server');
+    });
+
+    socket.on('disconnect', function () {
+        console.log('Disconnected from server');
+    });
+
+    socket.on('newMessage', function (data) {
+        console.log('New message', data);
+    });
+// })();
