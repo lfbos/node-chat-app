dc3a81b501e137a401daaa7f2f66a4c5c03efe03
Luis Boscan <luife642@gmail.com>
2017-07-12 22:17:22 -0400

    comments removed

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,12 @@
-// (function () {
-    var socket = io();
-    socket.on('connect', function () {
-        console.log('Connected to server');
-    });
+var socket = io();
+socket.on('connect', function () {
+    console.log('Connected to server');
+});
 
-    socket.on('disconnect', function () {
-        console.log('Disconnected from server');
-    });
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
 
-    socket.on('newMessage', function (data) {
-        console.log('New message', data);
-    });
-// })();
+socket.on('newMessage', function (data) {
+    console.log('New message', data);
+});
