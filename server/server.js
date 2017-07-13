9a0636773ab0dfb3a741f0d15fada6c282c68d8b
Luis Boscan <luife642@gmail.com>
2017-07-12 22:10:37 -0400

    newMessage and createMessage events created

--- server/server.js
+++ server/server.js
@@ -13,8 +13,23 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Sutano',
+        text: 'New message from sutano',
+        createdAt: 1234
+    });
+
+    socket.on('createEmail', (data) => {
+        console.log('Create email', data);
+    });
+
+    socket.on('createMessage', (data) => {
+        console.log('Create message', data);
+    });
+
     socket.on('disconnect', () => {
-       console.log('User was disconnected');
+        console.log('User was disconnected');
     });
 });
 
